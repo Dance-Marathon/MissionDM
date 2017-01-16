a3385a614fa009f35ce4115da11bd00ee878aa94
R. Alex Clark <aclark2089@gmail.com>
2017-01-16 13:14:46 -0500

    Fixed db reset script to correctly reset postgres server

--- reset_db.sh
+++ reset_db.sh
@@ -3,9 +3,7 @@
 # Reset the database by clearing all teams and previous participants
 # Uses the refresh.sql file included in the databases/ directory
 
-DIR=$(pwd)
-
-/etc/init.d/postgres reload
+/etc/init.d/postgresql reload
 psql -h localhost -U postgres -c "DROP DATABASE missiondm"
 psql -h localhost -U postgres -c "CREATE DATABASE missiondm"
 psql -h localhost -d missiondm -U postgres -f $DIR/src/app/database/refresh.sql
